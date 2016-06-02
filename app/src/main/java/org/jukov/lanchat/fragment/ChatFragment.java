96d3d60684180ba46d82332347db4574f09b3daf
AlexanderJukov <bitlo@nn.ru>
2016-06-02 13:43:30 +0600

    Added message copying

--- app/src/main/java/org/jukov/lanchat/fragment/ChatFragment.java
+++ app/src/main/java/org/jukov/lanchat/fragment/ChatFragment.java
@@ -1,5 +1,7 @@
 package org.jukov.lanchat.fragment;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.net.wifi.SupplicantState;
@@ -13,6 +15,7 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.jukov.lanchat.NavigationDrawerActivity;
 import org.jukov.lanchat.R;
@@ -65,7 +68,8 @@ public abstract class ChatFragment extends BaseFragment {
                 builder.setTitle(getString(R.string.choose_action))
                         .setItems(new String[]{
                                 getString(R.string.delete_message),
-                                getString(R.string.star_message)}, new DialogInterface.OnClickListener() {
+                                getString(R.string.star_message),
+                                getString(R.string.copy_text)}, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 ChatData chatData = chatAdapter.getItem(position);
@@ -76,6 +80,13 @@ public abstract class ChatFragment extends BaseFragment {
                                         chatAdapter.remove(position);
                                         break;
                                     case 1:
+
+                                        break;
+                                    case 2:
+                                        ClipboardManager clipboardManager = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                                        ClipData clipData = ClipData.newPlainText("LANChat", chatData.getText());
+                                        clipboardManager.setPrimaryClip(clipData);
+                                        Toast.makeText(getContext(), R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show();
                                         break;
                                 }
                             }
